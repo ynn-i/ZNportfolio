fb6c579fa8b145de9a1f9a0016d8e9fad3327254
진승현 <hynn@jinseunghyeon-ui-MacBookPro.local>
2025-01-19 15:53:05 +0900

    Fixes 상대 경로로 수정

--- javascript/common.js
+++ javascript/common.js
@@ -2,18 +2,19 @@ const headerHTML = `
 <header class="header">
   <div class="container">
     <h1 onclick="window.location.href='${
-      window.location.pathname === '/index.html'
-        ? 'index.html'
+      window.location.pathname === '/index.html' ||
+      window.location.pathname.endsWith('/repository-name/')
+        ? './index.html'
         : '../index.html'
     }'" class="h1">
-      <img src="public/icon/ZN-LOGO.svg" alt="" />UXUI PORTFOLIO
+      <img src="./public/icon/ZN-LOGO.svg" alt="" />UXUI PORTFOLIO
     </h1>
     <div class="btn-container">
       <button
         onclick="window.location.href='${
           window.location.pathname.includes('works')
-            ? '../works/list.html'
-            : 'works/list.html'
+            ? './list.html'
+            : './works/list.html'
         }'"
         class="btn-page title-m"
         aria-label="WORKS"
@@ -23,8 +24,8 @@ const headerHTML = `
       <button
         onclick="window.location.href='${
           window.location.pathname === '/about.html'
-            ? 'about.html'
-            : '../about.html'
+            ? './about.html'
+            : './about.html'
         }'"
         class="btn-page title-m"
         aria-label="ABOUT"
